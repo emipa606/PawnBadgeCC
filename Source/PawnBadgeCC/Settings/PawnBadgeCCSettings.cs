7a5a8db9454d427864b0b8f762498f27fbc8f28b
emipa606 <emil.offermo@litet.net>
2021-07-24 19:09:05 +0200

    Updated for 1.3

---
+++ Source/PawnBadgeCC/Settings/PawnBadgeCCSettings.cs
@@ -0,0 +1,35 @@
+﻿using Verse;
+
+namespace PawnBadgeCC
+{
+    /// <summary>
+    ///     Definition of the settings for the mod
+    /// </summary>
+    internal class PawnBadgeCCSettings : ModSettings
+    {
+        public bool DeutschesHeer;
+        public bool GDI;
+        public bool GDIClean;
+        public bool NOD;
+        public bool NODClean;
+        public bool SWIAArmy;
+        public bool SWIANavy;
+        public bool SWRebels;
+
+        /// <summary>
+        ///     Saving and loading the values
+        /// </summary>
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref GDI, "GDI");
+            Scribe_Values.Look(ref GDIClean, "GDIClean");
+            Scribe_Values.Look(ref NOD, "NOD");
+            Scribe_Values.Look(ref NODClean, "NODClean");
+            Scribe_Values.Look(ref SWIAArmy, "SWIAArmy");
+            Scribe_Values.Look(ref SWIANavy, "SWIANavy");
+            Scribe_Values.Look(ref SWRebels, "SWRebels");
+            Scribe_Values.Look(ref DeutschesHeer, "DeutschesHeer");
+        }
+    }
+}
